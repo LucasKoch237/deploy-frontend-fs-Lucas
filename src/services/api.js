ec7e7d527a27811a87b3b99b6cb69fbae988ae29
LucasK237 <lkoch@frba.utn.edu.ar>
2026-01-05 21:28:15 -0300

    Conexion de frontend a backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,5 @@
-const API_BASE_URL = "https://deploy-backend-fs.vercel.app/api";
+const API_BASE_URL = `${import.meta.env.VITE_API_URL}/api`;
+
 
 // Función para obtener el token de autenticación
 const getAuthToken = () => {
